69f74b5d80cdb53afc3b4c3b582b90c805163099
kun.chen <kun.chen@yintech.cn>
2022-10-25 18:32:50 +0800

    init: 项目初始化以及文件路径配置

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+﻿// vite.config.js
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import glob from "glob";
+
+// 保存每个页面的名称和路径，后面会用到
+const multiPage = {};
+// 保存页面文件路径
+const pageEntry = {};
+
+function getFile() {
+  const allEntry = glob.sync("/package/**/index.html");
+  allEntry.forEach((entry: string) => {
+    const pathArr = entry.split("/");
+    const name = pathArr[pathArr.length - 2];
+    multiPage[name] = {
+      name,
+      rootPage: `/package/${name}/index.html`,
+    };
+    pageEntry[name] = resolve(__dirname, `/package/${name}/index.html`);
+  });
+}
+// 调用一下
+getFile();
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: pageEntry,
+    },
+  },
+});
