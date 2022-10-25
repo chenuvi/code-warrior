457d51ee617c6d6f325c75c54c27dd6d50576283
kun.chen <kun.chen@yintech.cn>
2022-10-25 19:05:25 +0800

    Feat: preliminary state-pattern

---
+++ package/state-pattern/light.ts
@@ -0,0 +1,56 @@
+﻿// https://mp.weixin.qq.com/s/fvaGLHq1iGQtFHDxKad1SA
+interface Light {
+  currentState(): void;
+  offLightState(): void;
+}
+interface LightState {
+  light?(): object;
+  clickButton(): void;
+}
+class OffLightState implements LightState {
+  constructor(light) {
+    this.light = light;
+  }
+  clickButton() {
+    console.log("Turn on the low light");
+    this.light.setState(this.light.lowLightState);
+  }
+}
+class lowLightState implements LightState {
+  constructor(light) {
+    this.light = light;
+  }
+  clickButton() {
+    console.log("Turn on the strong light");
+    this.light.setState(this.light.strongLightState);
+  }
+}
+class strongLightState implements LightState {
+  constructor(light) {
+    this.light = light;
+  }
+  clickButton() {
+    console.log("Turn off the light");
+    this.light.setState(this.light.offLightState);
+  }
+}
+
+class Light {
+  constructor() {
+    this.offLightState = new OffLightState(this);
+    this.lowLightState = new LowLightState(this);
+    this.strongLightState = new StrongLightState(this);
+    this.currentState = this.offLightState;
+  }
+  setState(newState) {
+    this.currrentState = newState;
+  }
+  clickButton() {
+    this.currentState.clickButton();
+  }
+}
+let light = new Light();
+
+light.clickButton();
+light.clickButton();
+light.clickButton();
