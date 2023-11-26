7644f8169d1b4b6a3cbc4a4e03dcae4b7523e406
chenuvi <chenuvi@outlook.com>
2023-11-26 16:27:25 +0800

    Feat: 防抖 节流

---
+++ package/hand-write/throttle.js
@@ -0,0 +1,24 @@
+// fn 是需要执行的函数
+// wait 是时间间隔
+const throttle = (fn, wait = 50) => {
+  // 上一次执行 fn 的时间
+  let previous = 0
+  // 将 throttle 处理结果当作函数返回
+  return function(...args) {
+    // 获取当前时间，转换成时间戳，单位毫秒
+    let now = +new Date()
+    console.log("🚀 ~ file: throttle.js:10 ~ returnfunction ~ now:", now)
+    // 将当前时间和上一次执行函数的时间进行对比
+    // 大于等待时间就把 previous 设置为当前时间并执行函数 fn
+    if (now - previous > wait) {
+      previous = now
+      fn.apply(this, args)
+    }
+  }
+}
+
+// DEMO
+// 执行 throttle 函数返回新函数
+const betterFn = throttle(() => console.log('fn 函数执行了'), 1000)
+// 每 10 毫秒执行一次 betterFn 函数，但是只有时间差大于 1000 时才会执行 fn
+setInterval(betterFn, 10)
